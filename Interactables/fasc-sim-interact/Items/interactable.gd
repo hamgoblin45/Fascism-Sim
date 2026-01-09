138a4eaed7d5e14ea4cbf36e7c0794a333f7f403
Phoenix Williams <hamgoblin45@gmail.com>
2026-01-09 04:36:15 -0700

    Interact outline material

--- Interactables/fasc-sim-interact/Items/interactable.gd
+++ Interactables/fasc-sim-interact/Items/interactable.gd
@@ -1,16 +1,31 @@
 extends Area3D
 class_name Interactable
 
+const INTERACT_OUTLINE = preload("uid://c3rhvr6bla26v")
+
+
 @export var id: String = ""
 
 @export var interact_icon: Texture2D
 @export var interact_text: String
 
+var meshes: Array[MeshInstance3D]
+
 signal interacted(type: String, engaged: bool)
 
 func _ready() -> void:
 	EventBus.item_interacted.connect(_interacted)
+	EventBus.looking_at_interactable.connect(_look_at_interactable)
+	_set_meshes()
 
+func _set_meshes():
+	for sibling in get_parent().get_children():
+		if sibling is MeshInstance3D:
+			meshes.append(sibling)
+		else:
+			for child in sibling.get_children(true):
+				if child is MeshInstance3D:
+					meshes.append(child)
 
 func _interacted(_id: String, type: String, engaged: bool):
 	if _id != id:
@@ -21,3 +36,13 @@ func _interacted(_id: String, type: String, engaged: bool):
 	interacted.emit(type,engaged) # Connect to owner scene in order to handle unique functionaly
 	# i.e., turning on a light, playing a sound, etc
 	
+
+func _look_at_interactable(interact: Interactable, looking: bool):
+	if interact != self:
+		return
+	if looking:
+		for mesh in meshes:
+			mesh.material_overlay = INTERACT_OUTLINE
+	else:
+		for mesh in meshes:
+			mesh.material_overlay = null
