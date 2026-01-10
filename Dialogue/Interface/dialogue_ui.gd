b4b7bc4ca22f8424518f2c7d4bc17da7734d7707
Phoenix Williams <hamgoblin45@gmail.com>
2026-01-09 22:37:15 -0700

    Setting up code to display dialogue once there is some

--- Dialogue/Interface/dialogue_ui.gd
+++ Dialogue/Interface/dialogue_ui.gd
@@ -1,1 +1,14 @@
 extends Control
+
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		_start_dialogue()
+
+func _start_dialogue():
+	# Check if a dialogue is already running
+	if Dialogic.current_timeline != null:
+		return
+	
+	Dialogic.start("timeline")
+	get_viewport().set_input_as_handled()
