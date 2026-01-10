31817986ac286dfe425723f2e150fe9f4c6db55a
Phoenix Williams <hamgoblin45@gmail.com>
2026-01-10 14:49:03 -0700

    Added map project and basic map model

--- Dialogue/Interface/dialogue_ui.gd
+++ Dialogue/Interface/dialogue_ui.gd
@@ -1,5 +1,13 @@
 extends Control
 
+@onready var test_image_display: TextureRect = $TestImageDisplay
+
+
+
+func _ready():
+	## to TEST the ability to call funcs from dialogue
+	EventBus.dialogue_show_image.connect(_show_image)
+	EventBus.kick_balls.connect(_kicked_in_the_balls)
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
@@ -12,3 +20,12 @@ func _start_dialogue():
 	
 	Dialogic.start("timeline")
 	get_viewport().set_input_as_handled()
+
+
+## to TEST the ability to call funcs from dialogue
+func _show_image(image_path: String):
+	test_image_display.texture = load(image_path)
+	test_image_display.show()
+
+func _kicked_in_the_balls():
+	print("YOU WERE KICKED IN THE BALLS!@!!!!")
---
+++ Dialogue/Scripts/EventBus.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+signal dialogue_show_image(img: Texture2D)
+signal kick_balls
+
+
+#func _emit_from_dialogue
