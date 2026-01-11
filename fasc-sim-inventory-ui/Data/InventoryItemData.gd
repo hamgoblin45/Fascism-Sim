627cfc8b55a709919784c48a468024ea7037646c
Phoenix Williams <hamgoblin45@gmail.com>
2026-01-11 01:07:14 -0700

    Setting up inv_slot code

--- fasc-sim-inventory-ui/Data/InventoryItemData.gd
+++ fasc-sim-inventory-ui/Data/InventoryItemData.gd
@@ -4,3 +4,4 @@ class_name InventoryItemData
 @export var name: String = ""
 
 @export var texture: Texture2D
+@export var stackable: bool
--- fasc-sim-inventory-ui/Interface/inventory_slot_ui.gd
+++ fasc-sim-inventory-ui/Interface/inventory_slot_ui.gd
@@ -5,6 +5,8 @@ extends PanelContainer
 @onready var item_texture: TextureRect = %ItemTexture
 @onready var quantity: Label = %Quantity
 
+func _ready() -> void:
+	EventBus.removing_item_from_inventory.connect(_clear_slot_data)
 
 func _set_slot_data(new_slot_data: InventorySlotData):
 	slot_data = new_slot_data
@@ -13,6 +15,23 @@ func _set_slot_data(new_slot_data: InventorySlotData):
 	
 	item_texture.show()
 	item_texture.texture = slot_data.item_data.texture
-	if slot_data.quantity > 1:
+	if slot_data.quantity > 1 and slot_data.item_data.stackable:
 		quantity.show()
 		quantity.text = str(slot_data.quantity)
+	EventBus.inventory_item_updated.emit(slot_data)
+
+func _clear_slot_data(slot: InventorySlotData):
+	if slot.item_data and slot_data.item_data and slot.item_data == slot_data.item_data:
+		slot_data = null
+		item_texture.hide()
+		quantity.hide()
+		EventBus.inventory_item_updated.emit(slot_data)
+
+func _change_quantity(value: int):
+	slot_data.quantity += value
+	if slot_data.quantity > 1 and slot_data.item_data.stackable:
+		quantity.show()
+		quantity.text = str(slot_data.quantity)
+		EventBus.inventory_item_updated.emit(slot_data)
+	elif slot_data.quantity <= 0:
+		_clear_slot_data(slot_data)
