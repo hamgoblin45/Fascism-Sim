57f04cfb1be2aae9e2912c8714fd88dc6a2471b9
Phoenix Williams <hamgoblin45@gmail.com>
2026-01-12 19:48:11 -0700

    Test UI hooked up

--- fasc-sim-inventory-ui/test_stuff.gd
+++ fasc-sim-inventory-ui/test_stuff.gd
@@ -3,12 +3,18 @@ extends Control
 @onready var open_give_ui: Button = $TestPanel/VBoxContainer/OpenGiveUI
 @onready var get_item: Button = $TestPanel/VBoxContainer/GetItem
 
+
 var in_dialogue: bool = false
+
+func _ready() -> void:
+	EventBus.giving_item.connect(_on_give_item)
 
 func _on_open_give_ui_pressed() -> void:
 	in_dialogue = !in_dialogue
+	%DialogueLabel.visible = in_dialogue
 	if in_dialogue:
 		EventBus.dialogue_started.emit()
+		
 	else:
 		EventBus.dialogue_ended.emit()
 
@@ -18,3 +24,9 @@ func _on_get_item_pressed() -> void:
 	test_slot.item_data = preload("uid://fligqycw40pd")
 	#test_slot.item_data = preload("uid://bu7k1xa16ud57")
 	EventBus.adding_item_to_inventory.emit()
+
+func _on_give_item(slot: InventorySlotData):
+	%GiveLabel.show()
+	%GiveLabel.text = "You gave the npc a[n] %s" % slot.item_data.name
+	await get_tree().create_timer(2.0).timeout
+	%GiveLabel.hide()
