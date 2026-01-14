352b85264a6c8e1f14a407cc7ba9c6ebcecf06c2
Phoenix Williams <hamgoblin45@gmail.com>
2026-01-13 21:25:48 -0700

    Setting up testing time UI

---
+++ fasc-sim-time/Maps/time_control_ui.gd
@@ -0,0 +1,8 @@
+extends PanelContainer
+
+
+@onready var weekday_label: Label = %WeekdayLabel
+@onready var day_label: Label = %DayLabel
+@onready var time_label: Label = %TimeLabel
+@onready var speed_label: Label = %SpeedLabel
+@onready var speed_slider: HSlider = %SpeedSlider
--- fasc-sim-time/Scripts/GameState.gd
+++ fasc-sim-time/Scripts/GameState.gd
@@ -1,1 +1,9 @@
 extends Node
+
+
+
+var day: int = 1
+var hour: int = 8
+var minute: int = 0
+
+var time_speed: float = 1.0
