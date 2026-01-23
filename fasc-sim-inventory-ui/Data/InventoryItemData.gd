2ae3cf95a59500de7a966414046b1cdb482d9f5e
Phoenix Williams <hamgoblin45@gmail.com>
2026-01-22 21:46:26 -0700

    Added some vars to ItemData

--- fasc-sim-inventory-ui/Data/InventoryItemData.gd
+++ fasc-sim-inventory-ui/Data/InventoryItemData.gd
@@ -7,6 +7,13 @@ class_name InventoryItemData
 @export var description: String = ""
 @export var flavor_text: String = ""
 
+@export var buy_value: float = 0.0
+@export var sell_value: float = 0.0
+
+@export var concealability: float = 0.0 # 0 - 5 w/ 5 most concealable and 0 the least
+@export var contraband_level: int = 0 # 0 - Legal, 1 - Frowned Upon, 2 - Prohibited, 3 - Strictly Illegal, etc
+
+@export var size: int = 0 # 0 - 5 w/ 5 being largest
 @export var stackable: bool
 @export var max_stack_size: int = 99
 @export var useable: bool
--- fasc-sim-inventory-ui/Scripts/GameState.gd
+++ fasc-sim-inventory-ui/Scripts/GameState.gd
@@ -3,3 +3,6 @@ extends Node
 
 var able_to_give_item: bool = false
 var shopping: bool = false
+
+
+var money: float = 0
