e5f3b38918055450d7420e3ca42dac3fd88dcb81
Phoenix Williams <hamgoblin45@gmail.com>
2026-02-08 11:00:28 -0700

    MainMod: Set up WorldManager

--- fasc-sim-main-module/Scripts/Managers/WorldManager.gd
+++ fasc-sim-main-module/Scripts/Managers/WorldManager.gd
@@ -1,1 +1,33 @@
 extends Node
+
+var world_schedule = {
+	# EXAMPLE
+	# The day the following schedule occurs
+	# 2: {
+	#	"flags": {"checkpoint_vandalized": true, "teenager_hiding": true},
+	#	"description": "The checkpoint has been vandalized and the Enforcers are hunting the culprit"
+	#},
+	# ... # Add rest of days here
+}
+
+
+func _ready() -> void:
+	EventBus.day_changed.connect(_on_day_changed)
+	_apply_world_state(GameState.day)
+
+func _on_day_changed(new_day: int):
+	_apply_world_state(new_day)
+
+func _apply_world_state(day: int):
+	# Keep default if no changes
+	if not world_schedule.has(day):
+		return
+	
+	var state = world_schedule[day]
+	
+	if "flags" in state:
+		for flag in state["flags"]:
+			GameState.world_flags[flag] = state["flags"][flag]
+			EventBus.world_changed.emit(flag, state["flags"][flag])
+	
+	print("WorldManager: Day %s initiated. Description: %s" % [day, state.get("description", "")])
