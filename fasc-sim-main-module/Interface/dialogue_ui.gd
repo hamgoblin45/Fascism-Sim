3843cecc7e0ff290f6884fa5926bceb5ebbcef81
Phoenix Williams <hamgoblin45@gmail.com>
2026-02-07 22:03:25 -0700

    MainMod: Added Dialogic

---
+++ fasc-sim-main-module/Interface/dialogue_ui.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@onready var test_image_display: TextureRect = $TestImageDisplay
+
+
+
+func _ready():
+	## to TEST the ability to call funcs from dialogue
+	EventBus.dialogue_show_image.connect(_show_image)
+	EventBus.kick_balls.connect(_kicked_in_the_balls)
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		_start_dialogue()
+
+func _start_dialogue():
+	# Check if a dialogue is already running
+	if Dialogic.current_timeline != null:
+		return
+	
+	Dialogic.start("timeline")
+	get_viewport().set_input_as_handled()
+
+
+## to TEST the ability to call funcs from dialogue
+func _show_image(image_path: String):
+	test_image_display.texture = load(image_path)
+	test_image_display.show()
+
+func _kicked_in_the_balls():
+	print("YOU WERE KICKED IN THE BALLS!@!!!!")
