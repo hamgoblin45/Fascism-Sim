c41bf09e8c922044e7bbbb321656fd4a03bc7ae6
Phoenix Williams <hamgoblin45@gmail.com>
2026-02-11 12:31:23 -0700

    Fixed test containers not being interactable

--- fasc-sim-main-module/Interface/search_ui.gd
+++ fasc-sim-main-module/Interface/search_ui.gd
@@ -1,1 +1,5 @@
 extends Control
+
+@onready var raid_in_progress_label: Label = %RaidInProgressLabel
+@onready var answer_door_timer_label: Label = %AnswerDoorTimerLabel
+@onready var answer_door_timer: Timer = %AnswerDoorTimer
